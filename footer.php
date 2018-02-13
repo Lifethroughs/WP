1af2a02e559a5cde9254f43ef41d197267e14863
Cimi_MAC <35927985+Lifethroughs@users.noreply.github.com>
2018-02-13 20:33:05 +0200

    add bootstrap functionality

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
-            <footer>Copyright &copy; 2018</footer>
-        </div>
+            <footer class="blog-footer">
+                <p>&copy; <?php bloginfo('name');?> - <?php echo Date('Y'); ?></p>
+                <p> <a href="#">Back to top</a></p>
+            </footer>
+            <!-- Footer hook -->
+            <?php wp_footer(); ?>
+
+       <script src="<?php bloginfo('template_url'); ?>/assets/js/bootstrap.js">
+       </script>
     </body>
 </html>
--- header.php
+++ header.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 	<head>
+		<!-- Meta -->
+		<meta charset="<?php bloginfo('charset'); ?>">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="<?php bloginfo('description'); ?>">
+	    <meta name="author" content="Sami Suikki">
+
+		<!-- Fav icon -->
+	    <link rel="icon" href="<?php bloginfo('template_url')?>/assets/img/favicon.ico">
+
 		<title>
-			<?php
-				wp_title('|', true, right); //separator,display,seplocation
-				bloginfo('name'); //blog's name
-			?>
+			<?php bloginfo('name'); ?>
+			<?php is_front_page() ? bloginfo('description') : wp_title('|', true, left); ?>
 		</title>
-		<?php
-			wp_head(); //defining head hook for plugins
-		?>
+
+		<!-- CSS -->
+		<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+		<!-- Bootstrap CSS -->
+    	<link href="<?php bloginfo(template_url); ?>/assets/css/bootstrap.css" rel="stylesheet">
+
+		<!-- Head hook -->
+		<?php wp_head(); ?>
 	</head>
 	<body>
 		<div id="container">
@@ -20,3 +33,4 @@
 			</header>
 			<div style="clear:both;"></div>
 			<nav class="main"><?php wp_nav_menu(); ?></nav>
+		</div>
